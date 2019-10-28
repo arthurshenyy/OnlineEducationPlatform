72dfc49317b3d4c6003d3aab6a9dbdacd569a97f
dutlzn <2436013662@qq.com>
2019-10-28 20:05:00 +0800

    初始化新项目

---
+++ video/src/test/java/com/net/lznvideo/video/VideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.lznvideo.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
