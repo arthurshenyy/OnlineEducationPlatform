d455a0ba7014463a67a421ccd3a072c30ed581f3
dutlzn <2436013662@qq.com>
2019-10-30 21:06:20 +0800

    登录检验jwt封装通用方法

---
+++ video/src/test/java/com/net/lznvideo/video/CommonTest.java
@@ -0,0 +1,42 @@
+package com.net.lznvideo.video;
+
+import com.net.lznvideo.video.domain.User;
+import com.net.lznvideo.video.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import org.junit.Test;
+
+public class CommonTest {
+    @Test
+    public void testGeneJwt(){
+
+        User user = new User();
+        user.setId(999);
+        user.setHeadImg("www.xdclass.net");
+        user.setName("xd");
+
+        String token = JwtUtils.geneJsonWebToken(user);
+        System.out.println(token);
+
+    }
+
+
+    @Test
+    public void testCheck(){
+
+        String token = "1eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ4ZGNsYXNzIiwiaWQiOjk5OSwibmFtZSI6InhkIiwiaW1nIjoid3d3LnhkY2xhc3MubmV0IiwiaWF0IjoxNTI5NzYyMDczLCJleHAiOjE1MzAzNjY4NzN9.PNYfaeVA7UaaeUMOtpvxl9HRoj1ysdbK6weSzMGvfjc";
+        Claims claims = JwtUtils.checkJWT(token);
+        if(claims != null){
+            String name = (String)claims.get("name");
+            String img = (String)claims.get("img");
+            int id =(Integer) claims.get("id");
+            System.out.println(name);
+            System.out.println(img);
+            System.out.println(id);
+        }else{
+            System.out.println("非法token");
+        }
+
+
+    }
+
+}
--- video/src/test/java/com/net/lznvideo/video/VideoApplicationTests.java
+++ video/src/test/java/com/net/lznvideo/video/VideoApplicationTests.java
@@ -1,8 +1,12 @@
 package com.net.lznvideo.video;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class VideoApplicationTests {
 
